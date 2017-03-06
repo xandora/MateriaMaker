02a7871d5b72346ecb3744acc5fa8e128904e67a
michael <michael@X240-Laptop>
2017-03-07 12:39:23 +1300

    Test Commit from Eclipse

--- materiaMaker/src/xyz/xandora/MateriaMaker.java
+++ materiaMaker/src/xyz/xandora/MateriaMaker.java
@@ -5,31 +5,44 @@ public class MateriaMaker {
 	public static void main(String[] args) {
 
 		//Name, Type, Level, Grade, Class
-		materia Fire = new materia("Fire", "Green", 5, 3, "Fire");
-		materia Ice = new materia("Ice", "Green", 1, 5, "Ice");
-		materia Lightning = new materia("Lightning", "Green", 4, 5, "Lightning");
+		materia Fire = new materia(3, 3, "Fire");
+		materia Ice = new materia(4, 1, "Ice");
+		materia Lightning = new materia(4, 3, "Lightning");
+		materia Firaga = new materia(5, 5 ,"Fire");
+		materia Cure = new materia(3, 3, "Restorative");
+		materia Defend = new materia(5, 5, "Defensive");
 		
 		//Item Name, Item Grade
 		item Potion = new item("Potion", 1);
 		item MythrilArmlet = new item("Mythril Armlet", 7);
 		
 		//Test cases
-		materia.getMateria(Fire);
-		materia.getMateria(Ice);
-		materia.getMateria(Lightning);
-		fusion.ruleThree(Fire, Ice);
-		fusion.ruleThree(Lightning, Fire);
-		fusion.ruleThree(Ice, Lightning);
-		fusion.ruleThree(Ice, Ice);
-		fusion.ruleFive(Ice, Fire, MythrilArmlet, 1);
+//		materia.getMateria(Fire);
+//		materia.getMateria(Ice);
+//		materia.getMateria(Lightning);
+//		fusion.ruleThree(Fire, Ice);
+//		fusion.ruleThree(Lightning, Fire);
+//		fusion.ruleThree(Ice, Lightning);
+//		fusion.ruleThree(Ice, Ice);
+//		fusion.ruleFive(Ice, Fire, MythrilArmlet, 1);
 		
 		System.out.println();
 		System.out.println("Fire vs. Ice");
 		fusion.ruleOne(Fire, Ice);
-		fusion.ruleTwo(Fire, Ice);
-		fusion.ruleThree(Fire, Ice);
-		fusion.ruleFour(Fire, Ice);
-		fusion.ruleFive(Fire, Ice, MythrilArmlet, 1);
+
+		System.out.println();
+		System.out.println("Fire vs. Lightning");
+		fusion.ruleOne(Defend, Lightning);
+
+		System.out.println();
+		System.out.println("Fire vs. Firaga");
+		fusion.ruleOne(Fire, Cure);
+		
+		System.out.println();
+		materia.getMateriaName(Cure.materiaClass, Cure.materiaGrade);
+		materia.getMateriaName(Defend.materiaClass, Defend.materiaGrade);
+		
+		
 	}
 
 }
